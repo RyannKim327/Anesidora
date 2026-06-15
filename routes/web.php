d879b105f8174680aecb8a2282c949139721f2ab
RyannKim327 <rksesgundo123@gmail.com>
2026-06-16 07:06:34 +0800

    06-16-26  7:06 [feat: uploads]

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\UploadController;
+use Illuminate\Support\Facades\Route;
+
 foreach (glob(__DIR__.'/functions/*.php') as $file) {
     require $file;
 }
@@ -7,3 +10,6 @@ foreach (glob(__DIR__.'/functions/*.php') as $file) {
 foreach (glob(__DIR__.'/api/*.php') as $file) {
     require $file;
 }
+
+// Keeping the upload index route to show the page
+Route::get('/upload', [UploadController::class, 'index'])->name('upload.index');
